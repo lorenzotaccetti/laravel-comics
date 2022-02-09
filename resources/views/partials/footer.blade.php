2d87e4896ab1ab948c5f7568ad0faf54e773cccd
lorenzotaccetti <lorenzo.taccetti2001@gmail.com>
2022-02-09 17:03:32 +0100

    footer done

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,115 @@
-<h1>
-    Footer
-</h1>
+<footer>
+    {{-- Top-footer --}}
+    <div class="top-footer">
+        <div class="container position">
+            <div class="footer-links">
+                <ul>
+                    <h2>DC Comics</h2>
+                    <li>
+                        <a href="#">Characters</a>
+                    </li>
+                    <li>
+                        <a href="#">Comics</a>
+                    </li>
+                    <li>
+                        <a href="#">Movies</a>
+                    </li>
+                    <li>
+                        <a href="#">TV</a>
+                    </li>
+                    <li>
+                        <a href="#">Games</a>
+                    </li>
+                    <li>
+                        <a href="#">Videos</a>
+                    </li>
+                    <li>
+                        <a href="#">News</a>
+                    </li>
+                    <h2>Shop</h2>
+                    <li>
+                        <a href="#">Shop DC</a>
+                    </li>
+                    <li>
+                        <a href="#">Shop DC Collectibles</a>
+                    </li>
+                </ul>
+                <ul>
+                    <h2>DC</h2>
+                    <li>
+                        <a href="#">Terms Of Use</a>
+                    </li>
+                    <li>
+                        <a href="#">Privacy Police (New)</a>
+                    </li>
+                    <li>
+                        <a href="#">Ad Choices</a>
+                    </li>
+                    <li>
+                        <a href="#">Advertising</a>
+                    </li>
+                    <li>
+                        <a href="#">Jobs</a>
+                    </li>
+                    <li>
+                        <a href="#">Subscriptions</a>
+                    </li>
+                    <li>
+                        <a href="#">Talent Workshops</a>
+                    </li>
+                    <li>
+                        <a href="#">CPSC Certificates</a>
+                    </li>
+                    <li>
+                        <a href="#">Ratings</a>
+                    </li>
+                    <li>
+                        <a href="#">Shop Help</a>
+                    </li>
+                    <li>
+                        <a href="#">Contact Us</a>
+                    </li>
+                </ul>
+                <ul>
+                    <h2>Sites</h2>
+                    <li>
+                        <a href="#">DC</a>
+                    </li>
+                    <li>
+                        <a href="#">Mad Magazine</a>
+                    </li>
+                    <li>
+                        <a href="#">DC Kids</a>
+                    </li>
+                    <li>
+                        <a href="#">DC Universe</a>
+                    </li>
+                    <li>
+                        <a href="#">DC Power Visa</a>
+                    </li>
+                </ul> 
+            </div>
+            <div>
+                <img src="{{asset('images/dc-logo-bg.png')}}" alt="">
+            </div>
+        </div>
+    </div>
+
+    {{-- Bottom-footer --}}
+    <div class="bottom-footer">
+        <div class="container position">
+            <div class="btn">
+                <button>Sign-Up Now!</button>
+            </div>
+
+            <div class="socials">
+                <span>Follow Us</span>
+                <img src="{{asset('images/footer-facebook.png')}}" alt="">
+                <img src="{{asset('images/footer-twitter.png')}}" alt="">
+                <img src="{{asset('images/footer-youtube.png')}}" alt="">
+                <img src="{{asset('images/footer-pinterest.png')}}" alt="">
+                <img src="{{asset('images/footer-periscope.png')}}" alt="">
+            </div>
+        </div>
+    </div>
+</footer>
