63c1939207a899a4b07ea9cfd2a3aceba2b10a67
lorenzotaccetti <lorenzo.taccetti2001@gmail.com>
2022-02-09 16:35:53 +0100

    header done

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,66 @@
-<h1>
-    Header
-</h1>
+<header>
+    {{-- Top-header --}}
+    <div class="top-header">
+        <div class="container">
+            <span>
+                Dc power<i class="fas fa-trademark"></i>
+                visa<i class="far fa-registered"></i>
+            </span>
+            <span>
+                Additional dc sites
+                <i class="fas fa-sort-down arrow"></i>
+            </span>
+        </div>
+    </div>
+
+    {{-- Bottom-header --}}
+    <div class="bottom-header">
+        <div class="container position">
+            <div class="logo">
+                <img src="{{asset('images/dc-logo.png')}}" alt="">
+            </div>
+            <div>
+                <ul>
+                    <li>
+                        Characters
+                    </li>
+                    <li class="active">
+                        Comics
+                    </li>
+                    <li>
+                        Movies
+                    </li>
+                    <li>
+                        Tv
+                    </li>
+                    <li>
+                        Games
+                    </li>
+                    <li>
+                        Collectibles
+                    </li>
+                    <li>
+                        Videos
+                    </li>
+                    <li>
+                        Fans
+                    </li>
+                    <li>
+                        News
+                    </li>
+                    <li>
+                        Shop
+                    </li>
+                </ul>
+            </div>
+            <div>
+                <input type="text" placeholder="Search">
+                <i class="fas fa-search search"></i>
+            </div>
+        </div>
+    </div>
+
+    <div class="jumbotron">
+        <img src="{{asset('images/jumbotron.jpg')}}" alt="">
+    </div>
+</header>
