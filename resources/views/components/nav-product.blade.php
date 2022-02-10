9ccfbf692b621d1f7270fecfa260c5c1c570aa6c
lorenzotaccetti <lorenzo.taccetti2001@gmail.com>
2022-02-10 21:52:25 +0100

    rifiniture del layout

---
+++ resources/views/components/nav-product.blade.php
@@ -0,0 +1,38 @@
+<div class="nav-product">
+    <div class="container-product">
+        <div class="position">
+            <div class="box">
+                <div class="nav-title">
+                    Digital Comics
+                </div>
+                <div class="nav-img">
+                    <img src="{{asset('images/buy-comics-digital-comics.png')}}" alt="">
+                </div>
+            </div>
+            <div class="box">
+                <div class="nav-title">
+                    Shop DC
+                </div>
+                <div class="nav-img">
+                    <img src="{{asset('images/buy-comics-subscriptions.png')}}" alt="">
+                </div>
+            </div>
+            <div class="box">
+                <div class="nav-title">
+                    Comic Shop Locator
+                </div>
+                <div class="nav-img">
+                    <img src="{{asset('images/buy-comics-shop-locator.png')}}" alt="">
+                </div>
+            </div>
+            <div class="box right-border">
+                <div class="nav-title">
+                    Subscriptions
+                </div>
+                <div class="nav-img">
+                    <img src="{{asset('images/buy-comics-merchandise.png')}}" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -54,8 +54,62 @@
 
     <div class="bottom-product">
         <div class="container-product">
-
+            <div class="bottom-position">
+                <div class="talent-specs">
+                    <div class="bottom-title">
+                        Talent
+                    </div>
+                    <div class="bottom-table">
+                        <div class="left">
+                            Art by:
+                        </div>
+                        {{-- <div class="right blue">
+                            {{ $product['artists'] }}
+                        </div> --}}
+                    </div>
+                    <div class="bottom-table">
+                        <div class="left bottom-border">
+                            Written by:
+                        </div>
+                        {{-- <div class="right blue bottom-border">
+                            {{ $product['writers'] }}
+                        </div> --}}
+                    </div>
+                </div>
+                <div class="talent-specs">
+                    <div class="bottom-title">
+                        Specs
+                    </div>
+                    <div class="bottom-table">
+                        <div class="left">
+                            Series:
+                        </div>
+                        <div class="right series blue">
+                            {{ $product['type'] }}
+                        </div>
+                    </div>
+                    <div class="bottom-table">
+                        <div class="left">
+                            U.S. Price:
+                        </div>
+                        <div class="right">
+                            {{ $product['price'] }}
+                        </div>
+                    </div>
+                    <div class="bottom-table">
+                        <div class="left bottom-border">
+                            On Sale Date:
+                        </div>
+                        <div class="right bottom-border">
+                            {{ $product['sale_date'] }}
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
+
 </div>
+
+@include('components.nav-product')
 @endsection
